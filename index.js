6e1f3edf313a722654f132e8fd5e66bf6bb48840
threalwinky <voquangvu09112006@gmail.com>
2024-02-27 13:27:05 +0700

    u

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
-const { NlpManager } = require('node-nlp')
-const express = require('express');
-const mgr = new NlpManager(({ languages: ['en', 'vn'] }))
+const express = require('express')
 const app = express()
-const serverless = require('serverless-http')
-mgr.addDocument('en', 'hello', 'greeting')
-mgr.addDocument('en', 'hi', 'greeting')
-mgr.addDocument('en', 'good morning', 'greeting')
-mgr.addDocument('en', 'hey', 'greeting')
-mgr.addDocument('en', 'good afternoon', 'greeting')
 
-mgr.addDocument('en', 'fine', 'feeling')
-mgr.addDocument('en', 'good', 'feeling')
-mgr.addDocument('en', 'Feeling good', 'feeling')
-mgr.addDocument('en', `I'm fine`, 'feeling')
-
-mgr.addDocument('vn', 'xin chao', 'greeting-vn')
-mgr.addDocument('vn', 'chao', 'greeting-vn')
-
-
-
-mgr.addAnswer('en', 'greeting', 'Hi')
-mgr.addAnswer('en', 'greeting', 'Hi there')
-mgr.addAnswer('en', 'greeting', 'Hi! How are you? ')
-
-mgr.addAnswer('en', 'feeling', 'Great! What do you want')
-mgr.addAnswer('en', 'feeling', 'Great!')
-
-mgr.addAnswer('vn', 'greeting-vn', 'Xin chao')
-mgr.addAnswer('vn', 'greeting-vn', 'Chao ban')
-
-const router = express.Router()
-
-mgr.train().then(async () => {
-    mgr.save()
-    router.get('/bot', async(req, res) => {
-        let resp = await mgr.process('en', req.query.message)
-        res.send(resp.answer)
-    })
-    
-    app.use('/.netlify/',router)
-
-    app.listen(3000)
-
+app.get('/', async(req, res) => {
+    res.send('Working')
 })
 
-module.exports.handler = serverless(app)
+app.listen(3000, function(){
+    console.log('Server started')
+})
